39e6386165960caea6c0ab0cd83453c0764c8a23
Kenny Whyte <kawhyte@gmail.com>
2023-11-06 13:26:01 -0800

    .

--- components/Arenas.tsx
+++ components/Arenas.tsx
@@ -50,8 +50,8 @@ const Arenas = ({ arenas }) => {
 					animation={'/basketball.svg'}
 				/>
           
-
-          <div className="mt-14 container mx-auto grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+<div> </div>
+          <div className="mt-14 container  mx-auto px-6 place-items-stretch grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {arenas?.map((item) => (
               <div
                 key={item._id}
