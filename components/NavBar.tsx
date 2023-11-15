594c6a5e91fa2987770cd2ff1965b290550ef69c
Kenny Whyte <kawhyte@gmail.com>
2023-11-15 12:23:26 -0800

    fixed Hydration issue

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -169,12 +169,12 @@ export default function Nav({ color = 'bg-black', bgColor }) {
                             {item.name}
                           </div> */}
 
-                          <a
+                          <div
                className={classNames("inline-flex items-center px-1 pt-1 border-b-4 text-sm font-medium", router.route === item.href ?  `border-pink-500 text-gray-900` : "border-transparent text-gray-500  hover:text-gray-700")}
                aria-current={router.route === item.href ? "page" : undefined}
              >
                {item.name}
-             </a>
+             </div>
                         </div>
                       </Link>
                     ))}
