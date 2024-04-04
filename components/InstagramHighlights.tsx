bdb08b238cc5300dccbdf724b9fbe3b87fd39bb4
Kenny Whyte <kawhyte@gmail.com>
2024-04-04 10:58:35 -0700

    instagram update

--- components/InstagramHighlights.tsx
+++ components/InstagramHighlights.tsx
@@ -1,7 +1,7 @@
 import { video } from '@cloudinary/url-gen/qualifiers/source'
 import { oswald } from 'app/fonts'
 import dynamic from 'next/dynamic'
-
+import { InstagramEmbed } from 'react-social-media-embed'
 
 //  const url = encodeURIComponent("https://scontent-sjc3-1.cdninstagram.com/v/t51.29350-15/363827451_786834503238969_6827302780644465821_n.webp?stp=dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=POPCItyJEboAX9KBQbd&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AfDkaxgQL6p2RE-2G66Zh1Qhtac8cVRr1XOFVlO-1j3iKQ&oe=65247349")
 
@@ -9,10 +9,27 @@ const ReactPlayer = dynamic(() => import('react-player/youtube'), {
   ssr: false,
 })
 
+let featuredInstagram = [
+  {
+    url: 'https://www.instagram.com/p/C2TkjCHryfn/?next=%2Fmeet_the_whytes%2F',
+  },
+
+ 
+  {
+    url: 'https://www.instagram.com/p/C0LgXzauvCo/?next=%2Fmeet_the_whytes%2F',
+  },
+  {
+    url: 'https://www.instagram.com/p/CwtyimGOs2X/?next=%2Fmeet_the_whytes%2F&img_index=1',
+  },
+  {
+    url: 'https://www.instagram.com/p/CyUlhrRrUx1/?next=%2Fmeet_the_whytes%2F',
+  },
+  
+]
 
 const InstagramHighlights = ({ instagram }) => {
-//  console.log("Instgram URLLL", instagram)
-//  console.log("url ", url )
+  //  console.log("Instgram URLLL", instagram)
+  //  console.log("url ", url )
   return (
     <div className="container mx-auto  rounded-xl bg-yellow-50 px-10 py-12 ">
       <div className=" container mx-auto mb-10 flex w-full flex-wrap">
@@ -20,19 +37,27 @@ const InstagramHighlights = ({ instagram }) => {
           <h1
             className={`${oswald.variable}  title-font  mb-2 font-heading text-2xl font-medium text-gray-900 sm:text-3xl`}
           >
-            Latest Instagram Posts
+            Featured Instagram Posts
           </h1>
           <div className="h-1 w-20 rounded bg-pink-500"></div>
         </div>
       </div>
-      <div className="grid grid-cols-2 justify-items-center gap-5  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
-        {instagram &&
+      <div className="grid grid-cols-6 justify-items-center gap-5   md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4">
+      
+          {featuredInstagram?.map((item) => (
+            <div key={item.url} >
+              <InstagramEmbed  url={item.url} width={328}  />
+            </div>
+          ))}
+   
+
+        {/*  {instagram &&
           instagram.slice(0, 12).map(
             (item) =>
              
               item.media_type !== 'VIDEO' && (
                 <div key={item.id}>
-                  {/* <InstagramEmbed url={item.url} width={328} /> */}
+            
                   <a href={item.permalink}>
                     <img
                       className="aspect-square object-cover"
@@ -48,7 +73,7 @@ const InstagramHighlights = ({ instagram }) => {
 
   
 
-                  {/* <p className='truncate'>{item.caption}</p> */}
+             
 
 
                 </div>) ||
@@ -70,7 +95,7 @@ const InstagramHighlights = ({ instagram }) => {
 
 
              
-          )}
+          )} */}
       </div>
     </div>
   )
