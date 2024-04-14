f96f31480fb67bbce27668bc36a6f9b4ae6892af
Kenny Whyte <kawhyte@gmail.com>
2024-04-14 21:52:46 +0200

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,23 +7,27 @@ import { useEffect, useRef } from 'react'
 
 let animation = [
   {
-    name: 'Bath',
-    url: 'https://static3.lottiefiles.com/lotties/01_ramen_character.json',
+    name: 'watermelon',
+    url: 'watermelon.json',
+    // url: 'https://static3.lottiefiles.com/lotties/01_ramen_character.json',
     bg: ' bg-indigo-50',
   },
   {
     name: 'Bath',
-    url: 'https://lottie.host/f2f04f90-3319-4ccc-87c8-c7a609e54179/HfnKsEY8ST.json',
+    // url: 'https://lottie.host/f2f04f90-3319-4ccc-87c8-c7a609e54179/HfnKsEY8ST.json',
+    url: 'bath.json',
     bg: ' bg-indigo-50',
   },
   {
-    name: 'Bath',
-    url: 'https://lottie.host/abe85d6d-bbcd-42ca-aa12-6cab9a41c9f5/kpy5UMuVu8.json',
+    name: 'Ramen',
+    url: 'ramen.json',
+    // url: 'https://lottie.host/abe85d6d-bbcd-42ca-aa12-6cab9a41c9f5/kpy5UMuVu8.json',
     bg: ' bg-indigo-50',
   },
   {
-    name: 'Bath',
-    url: 'https://lottie.host/2ed631e4-1bf8-46bc-8702-bf19c933f21f/XN2AoNzMMY.json',
+    name: 'Sundae',
+    url: 'sundae.json',
+    // url: 'https://lottie.host/2ed631e4-1bf8-46bc-8702-bf19c933f21f/XN2AoNzMMY.json',
     bg: ' bg-indigo-50',
   },
 ]
@@ -55,18 +59,18 @@ function Hero() {
   // console.log("anim", animation[0].url)
   return (
     <>
-      <section className="body-font text-gray-600">
+      <section className="body-font container mx-auto max-w-6xl text-gray-600">
         <div className="container mx-auto flex flex-col  items-center px-5 md:pb-24 pt-12 md:flex-row">
-          <div className=" sm:mb-0 flex flex-col  items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-5">
+          <div className="sm:mb-0 flex flex-col  items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-5">
             <div
               className={`  ${oswald.variable}  flex flex-col -space-y-8    lg:-space-y-12  z-30 font-heading text-7xl  font-bold  uppercase text-gray-700  md:text-7xl lg:w-10/12 lg:text-[9rem]  xl:-space-y-20`}
             >
-              <p className="">Travel.</p>
-              <p className="text-pink-500">Food.</p>
+              <p className="">Food.</p>
+              <p className="text-pink-500"> Travel.</p>
               <p className="">Explore.</p>
             </div>
 
-            <p className="mb-8 text-lg xl:text-xl  leading-relaxed z-30">
+            <p className="mb-8 text-lg xl:text-xl max-w-2xl  leading-relaxed z-30">
               {' '}
               Hi! We are the Whytes. Welcome to our space on the{' '}
               <span className="text-pink-500">interwebs. </span> We&apos;re a
@@ -74,7 +78,7 @@ function Hero() {
               food.
             </p>
           </div>
-          <div className="hidden  -mt-12 md:mt-0  sm:block w-5/6 md:w-1/2 lg:w-full  lg:max-w-lg">
+          <div className="hidden max-w-sm  -mt-12 lg:-ml-10 md:mt-0  sm:block w-5/6 md:w-1/2 lg:w-full  lg:max-w-md">
           <PlayerWithNoSSR
                   autoplay
                   keepLastFrame
