60fc48016d996c1e6cb9c4e798534ac92caccf50
Kenny Whyte <kawhyte@gmail.com>
2025-08-30 18:48:38 -0700

    .

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -137,6 +137,7 @@ export default function Navbar() {
                           alt="MTW icon"
                           width={45}
                           height={45}
+                          priority
                         />
                         </span>
             </div>
