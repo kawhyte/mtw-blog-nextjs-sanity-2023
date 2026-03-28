469d3ea3e4e3837aef367a6f65891781490db0a5
Kenny Whyte <kawhyte@gmail.com>
2026-03-28 13:26:25 -0700

    fix: add favicon links to _document.tsx to ensure favicon appears on all pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&family=Raleway:wght@300;400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
       </Head>
       <body>
         <Main />
